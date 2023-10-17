8631c99007a5a11d1ad285ce7e92667e9d591e34
Emanuel Rojas <eroaguero01@gmail.com>
2023-10-16 19:28:33 -0600

    enable cors

--- backend/app.js
+++ backend/app.js
@@ -51,7 +51,13 @@ const limiter = rateLimit({
 app.use('/', limiter);
 
 //enable CORS
-// app.use(cors());
+app.use(
+  cors({
+    origin: 'https://fundamentes-dev.netlify.app', // Update with your frontend URL
+    methods: 'GET,POST,UPDATE,PATCH,DELETE', // Add other HTTP methods as needed
+    allowedHeaders: 'Content-Type, Authorization', // Add other headers as needed
+  })
+);
 
 //data sanitization against XSS
 app.use(xss());
